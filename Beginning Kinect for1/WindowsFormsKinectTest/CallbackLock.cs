78b23d0cdda1a7072ad6f8e1baff3bfd9db11961
canama <cobblesster@gmail.com>
2015-10-07 21:12:11 -0700

    bababab

--- Beginning Kinect for1/WindowsFormsKinectTest/CallbackLock.cs
+++ Beginning Kinect for1/WindowsFormsKinectTest/CallbackLock.cs
@@ -2,6 +2,7 @@
 // <copyright file="CallbackLock.cs" company="Microsoft">
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
+// bababbaba
 // -----------------------------------------------------------------------
 
 namespace Microsoft.Kinect.Toolkit
